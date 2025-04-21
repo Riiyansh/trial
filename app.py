f9d722bb998ccf1475262e457b618be3684709fb
Riyansh Chouhan <125203122+Riiyansh@users.noreply.github.com>
2025-04-21 11:26:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+
+import os
+from flask import Flask, render_template, request, redirect, send_from_directory
+from werkzeug.utils import secure_filename
+from model import *  # import your functions here
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    image_url = None
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            return redirect(request.url)
+        file = request.files['image']
+        if file.filename == '':
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            image_url = filepath
+            # Run your model logic here if needed
+    return render_template('index.html', image_url=image_url)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
